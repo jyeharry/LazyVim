fce799443769cf8912db0ff48ecad94c09f33bdd
Jye Harry <jyeharry21@hotmail.com>
2024-12-03 11:54:03 +1100

    Initial setup

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,33 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- Better buffer cycling and deleting
+map("n", "<C-c>", function()
+  Snacks.bufdelete()
+end, { desc = "Delete buffer" })
+map("n", "<C-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "<C-h>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+
+map({ "n", "o", "v" }, "H", "^", { desc = "Beginning of line" })
+map({ "n", "o", "v" }, "L", "$", { desc = "End of line" })
+
+map("n", "p", "p=`]", { desc = "Paste and match indentation" })
+map("n", "P", "P=`]", { desc = "Paste before and match indentation" })
+
+map("n", "<Space>", "za", { desc = "Toggle fold" })
+
+map("n", "U", "<C-r>", { desc = "Redo" })
+
+local wk = require("which-key")
+wk.add({
+  {
+    "<leader>y",
+    group = "Yank",
+    { "<leader>ya", "<cmd>let @+=expand('%:p')<CR>:echo 'Copied absolute path'<CR>", desc = "Absolute Path" },
+    { "<leader>yr", "<cmd>let @+=expand('%:.')<CR>:echo 'Copied relative path'<CR>", desc = "Relative Path" },
+    { "<leader>yd", "<cmd>let @+=expand('%:h:t')<CR>:echo 'Copied directory name'<CR>", desc = "Directory Name" },
+    { "<leader>yf", "<cmd>let @+=expand('%:t')<CR>:echo 'Copied filename'<CR>", desc = "Filename" },
+  },
+})
---
+++ lua/config/options.lua
@@ -0,0 +1,15 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.g.mapleader = "\\"
+vim.g.maplocalleader = "\\"
+vim.o.foldlevel = 999
+vim.o.iskeyword = "@,48-57,192-255"
+vim.o.mouse = ""
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.scrolloff = 999
+vim.o.signcolumn = "yes"
+vim.o.spell = false
+vim.o.virtualedit = "all"
+vim.o.wrap = true
