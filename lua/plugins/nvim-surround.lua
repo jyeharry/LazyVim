4b6909f3333ffb77a2a63e7ac2ce9e84502f9804
Jye Harry <jye.harry@nine.com.au>
2024-12-05 22:46:02 +1100

    Update plugins

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -3,6 +3,20 @@ return {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "gs",
+        normal_cur = "gss",
+        normal_line = "gS",
+        normal_cur_line = "gSS",
+        visual = "gS",
+        visual_line = "gSS",
+        delete = "gsd",
+        change = "gsc",
+        change_line = "gSc",
+      },
+    },
   },
 }
--- lua/plugins/obsidian-nvim.lua
+++ lua/plugins/obsidian-nvim.lua
@@ -1,8 +1,5 @@
 local functions = require("config.functions")
 
-if true then
-  return {}
-end
 return {
   {
     "epwalsh/obsidian.nvim",
@@ -27,24 +24,50 @@ return {
         end,
         desc = "Obsidian Follow Link",
       },
-      -- ["<leader>O"] = {
-      --   name = "Obsidian",
-      { "D", "<Cmd>ObsidianDailies<CR>", desc = "Dailies" },
-      { "T", "<Cmd>ObsidianTags<CR>", desc = "Tags" },
-      { "b", "<Cmd>ObsidianBacklinks<CR>", desc = "Backlinks" },
-      { "c", "<Cmd>ObsidianToggleCheckbox<CR>", desc = "Toggle Checkbox" },
-      { "d", "<Cmd>ObsidianToday<CR>", desc = "Today" },
-      { "f", "<Cmd>ObsidianQuickSwitch<CR>", desc = "Find notes" },
-      { "g", "<Cmd>ObsidianSearch<CR>", desc = "Grep" },
-      { "i", "<Cmd>ObsidianPasteImg<CR>", desc = "Paste Image" },
-      { "l", "<Cmd>ObsidianLinks<CR>", desc = "Links" },
-      { "n", "<Cmd>ObsidianNew<CR>", desc = "New Note" },
-      { "o", "<Cmd>ObsidianOpen<CR>", desc = "Open" },
-      { "p", "<Cmd>ObsidianTemplate<CR>", desc = "Insert Template" },
-      { "r", "<Cmd>ObsidianRename<CR>", desc = "Rename" },
-      { "t", "<Cmd>ObsidianTomorrow<CR>", desc = "Tomorrow" },
-      { "y", "<Cmd>ObsidianYesterday<CR>", desc = "Yesterday" },
-      -- },
+      { "<leader>O", "", desc = "Obsidian", mode = { "n", "v" } },
+      { "<leader>OD", "<Cmd>ObsidianDailies<CR>", desc = "Dailies" },
+      { "<leader>OT", "<Cmd>ObsidianTags<CR>", desc = "Tags" },
+      { "<leader>Ob", "<Cmd>ObsidianBacklinks<CR>", desc = "Backlinks" },
+      { "<leader>Oc", "<Cmd>ObsidianToggleCheckbox<CR>", desc = "Toggle Checkbox" },
+      { "<leader>Od", "<Cmd>ObsidianToday<CR>", desc = "Today" },
+      { "<leader>Of", "<Cmd>ObsidianQuickSwitch<CR>", desc = "Find notes" },
+      { "<leader>Og", "<Cmd>ObsidianSearch<CR>", desc = "Grep" },
+      { "<leader>Oi", "<Cmd>ObsidianPasteImg<CR>", desc = "Paste Image" },
+      { "<leader>Ol", "<Cmd>ObsidianLinks<CR>", desc = "Links" },
+      { "<leader>On", "<Cmd>ObsidianNew<CR>", desc = "New Note" },
+      { "<leader>Oo", "<Cmd>ObsidianOpen<CR>", desc = "Open" },
+      { "<leader>Op", "<Cmd>ObsidianTemplate<CR>", desc = "Insert Template" },
+      { "<leader>Or", "<Cmd>ObsidianRename<CR>", desc = "Rename" },
+      { "<leader>Ot", "<Cmd>ObsidianTomorrow<CR>", desc = "Tomorrow" },
+      { "<leader>Oy", "<Cmd>ObsidianYesterday<CR>", desc = "Yesterday" },
+      {
+        "e",
+        function()
+          functions.prompt_command(
+            "ObsidianExtractNote",
+            "Enter title of note to extract to (blank to use selection): ",
+            functions.getVisualSelection()
+          )
+        end,
+        "Extract",
+        mode = { "v" },
+      },
+      {
+        "l",
+        function()
+          functions.prompt_command("ObsidianLink", "Enter title of note to link to (blank to use selection): ")
+        end,
+        "Link",
+        mode = { "v" },
+      },
+      {
+        "n",
+        function()
+          functions.prompt_command("ObsidianLinkNew", "Enter title of new note (blank to use selection): ")
+        end,
+        "New Note",
+        mode = { "v" },
+      },
     },
     opts = {
       dir = vim.env.HOME .. "/obsidian-vault", -- specify the vault location. no need to call 'vim.fn.expand' here
@@ -73,6 +96,85 @@ return {
       -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
       -- URL it will be ignored but you can customize this behavior here.
       follow_url_func = vim.ui.open,
+    },
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    opts = {
+      dir = vim.env.HOME .. "/second-brain", -- specify the vault location. no need to call 'vim.fn.expand' here
+      use_advanced_uri = true,
+      finder = "telescope.nvim",
+      note_id_func = function(title)
+        return title
+      end,
+
+      templates = {
+        subdir = "templates",
+        date_format = "%-d of %B, %Y",
+        time_format = "%I:%M%p",
+      },
+
+      notes_subdir = "inbox",
+
+      mappings = {
+        ["gf"] = {
+          action = function()
+            if require("obsidian").util.cursor_on_markdown_link() then
+              return "<Cmd>ObsidianFollowLink<CR>"
+            else
+              return "gf"
+            end
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+          desc = "Obsidian Follow Link",
+        },
+        ["cx"] = {
+          action = "<Cmd>ObsidianToggleCheckbox<CR>",
+          desc = "Toggle checkbox",
+        },
+        ["<leader>Ox"] = {
+          action = "<Cmd>ObsidianToggleCheckbox<CR>",
+          desc = "Toggle checkbox",
+        },
+      },
+
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = "daily",
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = "%Y-%m-%d",
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        alias_format = "%B %-d, %Y",
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        -- template = 'daily.md',
+      },
+
+      note_frontmatter_func = function(note)
+        -- This is equivalent to the default frontmatter function.
+        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+        -- `note.metadata` contains any manually added fields in the frontmatter.
+        -- So here we just make sure those fields are kept in the frontmatter.
+        if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+        return out
+      end,
+
+      -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+      -- URL it will be ignored but you can customize this behavior here.
+      follow_url_func = vim.ui.open or function(url)
+        require("astrocore").system_open(url)
+      end,
+
+      ui = {
+        checkboxes = {
+          [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+          ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+          ["x"] = { char = "", hl_group = "ObsidianDone" },
+        },
+      },
     },
   },
 }
