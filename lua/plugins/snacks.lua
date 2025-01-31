d1b3346824f50320bb1eac68600877aaae244580
Jye Harry <jye.harry@nine.com.au>
2025-01-31 12:43:27 +1100

    Update Lazyvim

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,11 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      picker = {
+        matcher = {
+          frecency = true, -- frecency bonus
+        },
+      },
       scroll = {
         animate = {
           duration = { step = 10, total = 100 },
