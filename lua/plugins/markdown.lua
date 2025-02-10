feca01aeebc22733fac493fb1eac104b0f18f170
Jye Harry <jyeharry21@hotmail.com>
2025-02-10 22:31:42 +1100

    Update lazyvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "williamboman/mason.nvim",
+    optional = true,
+    opts = {
+      ensure_installed = {
+        "mdx-analyzer",
+      },
+    },
+  },
+  {
     "mfussenegger/nvim-lint",
     optional = true,
     opts = {
