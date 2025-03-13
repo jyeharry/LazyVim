71751b7e15798d7a932d0b8b203c847bd8f2e44c
Jye Harry <jye.harry@nine.com.au>
2025-03-13 11:22:06 +1100

    Add rooter function

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local functions = require("config.functions")
+
+local root_augroup = vim.api.nvim_create_augroup("MyAutoRoot", {})
+vim.api.nvim_create_autocmd("BufEnter", { group = root_augroup, callback = functions.set_root })
--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -1,3 +1,9 @@
+-- Array of file names indicating root directory. Modify to your liking.
+local root_names = { ".git", "Makefile" }
+
+-- Cache to use for speed up (at cost of possibly outdated results)
+local root_cache = {}
+
 local Functions = {
   prompt_command = function(command, prompt, fallback)
     local user_input = vim.fn.input(prompt)
@@ -22,6 +28,29 @@ local Functions = {
     else
       return ""
     end
+  end,
+
+  set_root = function()
+    -- Get directory path to start search from
+    local path = vim.api.nvim_buf_get_name(0)
+    if path == "" then
+      return
+    end
+    path = vim.fs.dirname(path)
+
+    -- Try cache and resort to searching upward for root directory
+    local root = root_cache[path]
+    if root == nil then
+      local root_file = vim.fs.find(root_names, { path = path, upward = true })[1]
+      if root_file == nil then
+        return
+      end
+      root = vim.fs.dirname(root_file)
+      root_cache[path] = root
+    end
+
+    -- Set current directory
+    vim.fn.chdir(root)
   end,
 }
 
