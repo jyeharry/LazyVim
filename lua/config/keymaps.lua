adaf6c404b611f3dc4e295fae21d38d6553c34de
Jye Harry <jye.harry@meandem.com>
2025-10-02 17:01:03 +0100

    delete unused keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,13 +10,6 @@ end, { desc = "Delete buffer" })
 map("n", "<C-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 map("n", "<C-h>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
 
--- Window mappings
-vim.keymap.del("n", "<C-_>")
-map("n", "<C-.>", "2<C-w>>", { desc = "Increase window width" })
-map("n", "<C-,>", "2<C-w><", { desc = "Decrease window width" })
-map("n", "<C-S-=>", "2<C-w>+", { desc = "Increase window height", remap = false })
-map("n", "", "2<C-w>-", { desc = "Decrease window height", remap = false })
-
 map({ "n", "o", "v" }, "H", "^", { desc = "Beginning of line" })
 map({ "n", "o", "v" }, "L", "$", { desc = "End of line" })
 
