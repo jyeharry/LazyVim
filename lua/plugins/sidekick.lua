712c60c6dbf000ea131d788e5a65296f793981eb
Jye Harry <jye.harry@meandem.com>
2025-10-22 17:17:31 +0100

    update lazyvim

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -1,29 +1,43 @@
 return {
   "folke/sidekick.nvim",
-  -- stylua: ignore
   keys = {
     {
       "<leader>aa",
-      function() require("sidekick.cli").toggle({ name = "opencode", focus = true }) end,
+      function()
+        require("sidekick.cli").toggle({ name = "opencode", focus = true })
+      end,
       desc = "Sidekick OpenCode Toggle",
     },
     {
       "<leader>aT",
-      function() require("sidekick.nes").toggle() end,
+      function()
+        require("sidekick.nes").toggle()
+      end,
       mode = { "n" },
       desc = "Toggle NES",
     },
     {
       "<leader>au",
-      function() require("sidekick.nes").update() end,
+      function()
+        require("sidekick.nes").update()
+      end,
       mode = { "n" },
       desc = "Update NES",
     },
     {
       "<leader>ac",
-      function() require("sidekick.nes").clear() end,
+      function()
+        require("sidekick.nes").clear()
+      end,
       mode = { "n" },
       desc = "Clear NES",
+    },
+    {
+      "<leader>as",
+      function()
+        require("sidekick.cli").select({ filter = { installed = true } })
+      end,
+      desc = "Select CLI",
     },
   },
 }
