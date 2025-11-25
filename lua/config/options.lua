cdddb17562a52d96ee0de3eddc58092af1e6b9a6
Jye Harry <jye.harry@meandem.com>
2025-11-25 09:21:23 +0000

    update root_spec to account for monorepo root

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,7 +7,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.lazyvim_prettier_needs_config = true
 vim.g.ai_cmp = true
--- vim.g.root_spec = { { ".git", "lua" }, "cwd" }
+vim.g.root_spec = { { "pnpm-workspace.yaml", "turbo.json" }, "lsp", { ".git", "lua" }, "cwd" }
 -- vim.g.snacks_animate = false
 
 -- Global options
